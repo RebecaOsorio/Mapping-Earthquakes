a53db1822fd4771c1c411b701a64c4e76cacfd77
Rebeca Osorio González <90414330+RebecaOsorio@users.noreply.github.com>
2022-03-07 13:16:23 -0600

    Mapping a Route with Multiple Lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,12 +2,16 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+let map = L.map('mapid').setView([37.6213, -92.3790], 4);
 
 // Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
 let line = [
-    [33.9416, -118.4085],
-    [37.6213, -122.3790]
+    [37.6246436,-122.3983859],
+    [19.4360812,-99.074097],
+    [30.1974757,-97.6685416],
+    [43.6740598,-79.6315379],
+    [40.6413153,-73.780327]
   ];
 
 function numberWithCommas(x) {
@@ -16,7 +20,12 @@ function numberWithCommas(x) {
 
 // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
-    color: "red"
+    weight: 2,
+    opacity: 1,
+    color: 'blue',
+    dashArray: '4',
+    fillOpacity: 0.5,
+    fillColor: '#666666'
   }).addTo(map);
  
 // We create the tile layer that will be the background of our map.
@@ -24,7 +33,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style}/tiles
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY,
-    style:'dark-v10'
+    style:'light-v10'
 });
 
 // Then we add our 'graymap' tile layer to the map.
